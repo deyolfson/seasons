2d67070983d034d1ac0a2907258eac80738af0ac
deyolfson <deyolfson@gmail.com>
2015-03-22 15:27:21 -0700

    Initial Setup Files
    You have to start some where.

---
+++ midterm/js/sketch.js
@@ -0,0 +1,34 @@
+// David Eyolfson & Hannah E Smith
+// MidTerm Generative Code Spring 2015 -- Ryan Berkey
+
+//----------- Gobal Vars ----------------
+
+
+
+//----------- Preload Functions ----------------
+
+function preload(){
+    
+}
+
+function setup(){
+   
+   
+   
+}
+
+
+
+function draw() {
+
+}
+
+
+
+
+
+
+
+
+
+
